095c6981ca7d97f46539ffa0aa4a236d3451bfc6
the-snaake <petrovsemen444@gmail.com>
2022-03-06 19:25:01 +0500

    save-4

--- C++/[1] Find discriminant.cpp
+++ C++/[1] Find discriminant.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 int main(){
 float a,b,c,x,x1,x2,D;
-cout  << "vedite a b c" <<  endl;
+cout  << "Enter = a, b, c " <<  endl;
 cin  >>  a  >>  b  >>  c;
 if(a==0 and b==0){
 cout << "No sqrt" << endl;
 }
 else if (a==0){
 x=-c/b;
-cout  << "x=" << x << endl;
+cout  << "x= " << x << endl;
 }
 else{
 D=(pow(b,2))-(4*a*c);
@@ -21,11 +21,11 @@ D=(pow(b,2))-(4*a*c);
 if (D>0) {
 x1=(-b+sqrt(D))/(2*a);
 x2=(-b-sqrt(D))/(2*a);
-cout  <<  "x1=" <<  x1  << "x2=" <<  x2  << endl;
+cout  <<  "x1= " <<  x1  << "x2= " <<  x2  << endl;
 }
 else if (D==0){
 x=-b/(2*a);
-cout << "x=" <<  x  <<  endl;
+cout << "x= " <<  x  <<  endl;
 }
 else {
 cout << "no sqrt" <<  endl;
