6bb765405d8e35319099c0c7bd928d647b1bb858
the-snaake <petrovsemen444@gmail.com>
2022-03-08 21:10:13 +0500

    main

--- C++/[2] Calculator.cpp
+++ C++/[2] Calculator.cpp
@@ -4,44 +4,64 @@
 
 using namespace std;
 
-int main(){
-
-int a, b, action;
-char var;
-
-do{
-cout << "Enter 2 numbers: " << endl;
-cin >> a >> b;
-
-cout << "Сhoose an action: " << endl <<
-"1. Addition (+)"<< endl <<
-"2. Subtraction (-)" << endl <<
-"3. Multiplication (*)" << endl <<
-"4. Division (/)" << endl;
-cin >> action;
-
-switch (action)
+int main()
 {
-case 1:
-cout << "Result of addition: " << a + b << endl;
-break;
-case 2:
-cout << "Result of subtraction: " << a - b << endl;
-break;
-case 3:
-cout << "Result of multiplication: " << a * b << endl;
-break;
-case 4:
-cout << "Result of division:  " << (float) a / b << endl;
-break;
 
-default:
-cout << "Error" << endl;
-break;
-}
-cout << "Do you want to continue? (y/n)" << endl;
-cin >> var;
-}
-while (var == 'y');
+    int a, b, action;
+    char var;
 
+    cout << "Do you want to start program? (y/n)" << endl;
+    cin >> var;
+
+    while (var == 'y')
+    {
+
+        cout << "Enter 2 numbers: " << endl;
+        cin >> a >> b;
+
+        cout << "Сhoose an action: " << endl
+             << "1. Addition (+)" << endl
+             << "2. Subtraction (-)" << endl
+             << "3. Multiplication (*)" << endl
+             << "4. Division (/)" << endl
+             << "5. Exponentiation (^)" << endl
+             << "6. Operation (x)" << endl;
+        cin >> action;
+
+        switch (action)
+        {
+        case 1:
+            cout << "Result of addition: " << a + b << endl;
+            break;
+        case 2:
+            cout << "Result of subtraction: " << a - b << endl;
+            break;
+        case 3:
+            cout << "Result of multiplication: " << a * b << endl;
+            break;
+        case 4:
+            if (b != 0)
+            {
+                cout << "Result of division:  " << (float)a / b << endl;
+            }
+            else
+            {
+                cout << "Error." << endl;
+            }
+            break;
+        case 5:
+            cout << "Result of exponentiation: " << pow(a, b) << endl;
+            break;
+        case 6:
+            cout << "Result of operation:  "
+                 << "Operation" << endl;
+            break;
+
+        default:
+            cout << "Sorry man, but you can't use that symbol!" << endl;
+            break;
+        }
+        cout << "Do you want to continue? (y/n)" << endl;
+        cin >> var;
+    }
 }
