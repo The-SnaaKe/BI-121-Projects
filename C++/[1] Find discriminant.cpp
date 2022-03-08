b4678399d8ceff16b1d34841dd7a73406ee098a3
the-snaake <petrovsemen444@gmail.com>
2022-03-08 21:40:43 +0500

    main

--- C++/[1] Find discriminant.cpp
+++ C++/[1] Find discriminant.cpp
@@ -30,7 +30,7 @@ int main()
         {
             x1 = (-b + sqrt(D)) / (2 * a);
             x2 = (-b - sqrt(D)) / (2 * a);
-            cout << "x1= " << x1 << "x2= " << x2 << endl;
+            cout << " x1= " << x1 << endl << " x2= " << x2 << endl;
         }
         else if (D == 0)
         {
