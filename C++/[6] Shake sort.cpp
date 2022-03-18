c93c4c7762357a3e0ffc2c48c03885e8bfe36c4a
the-snaake <petrovsemen444@gmail.com>
2022-03-18 20:13:51 +0500

    save--kr

--- C++/[6] Shake sort.cpp
+++ C++/[6] Shake sort.cpp
@@ -7,41 +7,42 @@ using namespace std;
 int main()
 {
     int n, i, left, right;
-
+    
     cout << "Enter n - ";
     cin >> n;
     int a[n];
-
+    srand(time(NULL));
+    left = 0;
+    right = n - 1;
+    
     cout << "Before massiv: ";
     for (i = 0; i < n; i++)
     {
         a[i] = rand() % 41 - 19;
         cout << a[i] << " ";
     }
-
+    
     while (left < right)
     {
-        srand(time(NULL));
-        left = 0;
-        right = n - 1;
-        for (i = 0; i < n; i++)
+        for (i = left; i < right; i++)
         {
             if (a[i] > a[i + 1])
                 swap(a[i], a[i + 1]);
-                right--;
         }
+        right--;
+        
         for (i = right; i > left; i--)
         {
             if (a[i] < a[i - 1])
-                swap(a[i], a[i + 1]);
-                left++;
+                swap(a[i], a[i - 1]);
         }
+        left++;
     }
     cout << endl;
     cout << "After massiv: ";
     for (i = 0; i < n; i++){
         cout << a[i] << " ";
     }
-    return(0);
+    cout << endl;
+    system("pause");
 }
- 
